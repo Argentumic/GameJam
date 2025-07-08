20a6356263b59ea902c42341626652be833e1d36
Argentumic <artemtarabasyt@gmail.com>
2025-07-08 20:56:17 +0300

    fan now exists

---
+++ fan.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var fan_speed = -200
+var is_moved = false
+
+func _on_blow_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_moved = true
+
+
+func _on_blow_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_moved = false
+
+func _physics_process(delta: float) -> void:
+	if get_node("../Player").state == 0:
+		fan_speed = -10
+	elif get_node("../Player").state == 1:
+		fan_speed = -200
+	else:
+		fan_speed = -149
+		
+	if is_moved:
+		get_node("../Player").position.x += fan_speed*delta
