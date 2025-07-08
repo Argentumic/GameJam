da5f13bb43b410c8977343801ed6be3f604a1807
Argentumic <artemtarabasyt@gmail.com>
2025-07-08 15:49:18 +0300

    jump buffer and state change

--- Jump.gd
+++ Jump.gd
@@ -9,10 +9,16 @@ extends Node
 const COYOTE_TIMER_MAX: int = 6
 var coyote_timer: int = 0
 
+const BUFFER_TIMER_MAX: int = 6
+var buffer_timer: int = 0
+var holded_time: float = 0
+
 const VARIABLE_JUMP_TIMER_MAX: int = 5
 var variable_jump_timer: int = 0
 
 var can_jump: bool = false
+
+var close_to_ground: bool = false
 
 func _physics_process(delta: float) -> void:
 	
@@ -29,20 +35,39 @@ func _physics_process(delta: float) -> void:
 		
 	if !get_parent().is_on_floor() and coyote_timer>0 and !Input.is_action_pressed("Jump"):
 		can_jump = true
-		coyote_timer -= 1
+		coyote_timer -= delta
 	elif !get_parent().is_on_floor() and coyote_timer==0:
 		can_jump = false
-	print(coyote_timer, " " ,can_jump, " " ,variable_jump_timer)
 	
 	if can_jump and Input.is_action_pressed("Jump") and variable_jump_timer>-1:
-		if variable_jump_timer == VARIABLE_JUMP_TIMER_MAX:
-			get_parent().velocity.y = JUMP_VELOCITY_START
-			variable_jump_timer -= 1
-		elif variable_jump_timer > 0 and variable_jump_timer < VARIABLE_JUMP_TIMER_MAX:
-			get_parent().velocity.y += JUMP_VELOCITY_CONTINUOUS
-			variable_jump_timer -= 1
-		else:
-			can_jump = false
+		jump(delta)
 			
 	if Input.is_action_just_released("Jump"):
+		can_jump = false
+		
+	if Input.is_action_just_pressed("Jump") and !get_parent().is_on_floor():
+		buffer_timer = BUFFER_TIMER_MAX
+		holded_time = 0
+	
+	if Input.is_action_pressed("Jump") and !get_parent().is_on_floor():
+		holded_time += 1
+	
+	if !get_parent().is_on_floor() and buffer_timer > 0:
+		buffer_timer -= delta
+	
+	if get_parent().is_on_floor() and buffer_timer > 0:
+		get_parent().velocity.y = JUMP_VELOCITY_START
+		variable_jump_timer = VARIABLE_JUMP_TIMER_MAX
+		can_jump = true
+		buffer_timer = 0
+	
+	
+func jump(delta: float):
+	if variable_jump_timer == VARIABLE_JUMP_TIMER_MAX:
+		get_parent().velocity.y = JUMP_VELOCITY_START
+		variable_jump_timer -= delta
+	elif variable_jump_timer > 0 and variable_jump_timer < VARIABLE_JUMP_TIMER_MAX:
+		get_parent().velocity.y += JUMP_VELOCITY_CONTINUOUS
+		variable_jump_timer -= delta
+	else:
 		can_jump = false
