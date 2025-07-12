cadc205b66a12cdffa08f7d1198aab96ad27846c
Argentumic <106577953+Argentumic@users.noreply.github.com>
2025-07-12 11:13:09 +0300

    corrected mesh

--- scripts/mesh.gd
+++ scripts/mesh.gd
@@ -3,9 +3,9 @@ extends StaticBody2D
 var is_player_inside: bool = false
 
 func _process(delta: float) -> void:
-	if is_player_inside and get_node("../Player").state == 0:
+	if is_player_inside and $"../../Player".state == 0:
 		get_tree().reload_current_scene()
-	elif get_node("../Player").state == 0:
+	elif $"../../Player".state == 0:
 		get_node("CollisionShape2D").position.y = 0
 	else:
 		get_node("CollisionShape2D").position.y = 1000000
