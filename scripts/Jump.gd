445f23b2d4267917aa6a2d0553f7c257a117de33
Argentumic <artemtarabasyt@gmail.com>
2025-07-13 23:54:44 +0300

    release candidate

--- scripts/Jump.gd
+++ scripts/Jump.gd
@@ -4,9 +4,13 @@ extends Node
 @export var gravity_coef: float = 1.0
 @export var gravity_fall_coef: float = 2.0
 @export var jump_cut_coef: float = 0.5
+var jumpSFX = preload("res://SFX/jump.ogg")
+var landingSFX = preload("res://SFX/drop.ogg")
 
 var is_ascending: bool = false
 var is_cutted: bool = false
+var is_landed: bool = true
+var is_first_time_landed: bool = true
 
 const COYOTE_TIMER_MAX: int = 6
 var coyote_timer: int = 0
@@ -32,17 +36,27 @@ func _physics_process(delta: float) -> void:
 	
 	if get_parent().is_on_floor():
 		coyote_timer = COYOTE_TIMER_MAX
+		if !is_landed and !is_first_time_landed and get_parent().state != 2:
+			$"../SFXplayer".stream = landingSFX
+			$"../SFXplayer".pitch_scale = randf_range(0.9,1.1)
+			$"../SFXplayer".play()
+		is_landed = true
+		is_first_time_landed = false
 	else:
 		coyote_timer -= delta
+		is_landed = false
 	
 	#COYOTE TIME ENDS
 	#MAIN JUMP PART STARTS
 	
 	if (get_parent().is_on_floor() or coyote_timer > 0) and Input.is_action_just_pressed("Jump"):
 		get_parent().velocity.y = JUMP_VELOCITY
+		$"../SFXplayer".stream = jumpSFX
+		$"../SFXplayer".pitch_scale = randf_range(0.9,1.1)
+		$"../SFXplayer".play()
 		is_cutted = false
 	
-	if Input.is_action_just_released("Jump") and !get_parent().is_on_floor() and !is_cutted:
+	if Input.is_action_just_released("Jump") and !get_parent().is_on_floor() and !is_cutted and is_ascending:
 		get_parent().velocity.y *= jump_cut_coef
 		is_cutted = true
 			
@@ -57,4 +71,7 @@ func _physics_process(delta: float) -> void:
 	
 	if get_parent().is_on_floor() and buffer_timer > 0:
 		get_parent().velocity.y = JUMP_VELOCITY
+		$"../SFXplayer".stream = jumpSFX
+		$"../SFXplayer".pitch_scale = randf_range(0.9,1.1)
+		$"../SFXplayer".play()
 		buffer_timer = 0
